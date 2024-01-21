865d26b1513102ef4957dd68511d4a502c1779d8
Aviad Yarkoni <Aviad.Yarkoni@outlook.co.il>
2024-01-21 16:56:47 +0200

    upload terraform var files and ec2

--- create_jenkins_server/ec2.tf
+++ create_jenkins_server/ec2.tf
@@ -5,9 +5,7 @@ resource "aws_instance" "ec2" {
   subnet_id              = aws_subnet.public-subnet.id
   vpc_security_group_ids = [aws_security_group.security-group.id]
   iam_instance_profile   = aws_iam_instance_profile.instance-profile.name
-  root_block_device {
-    volume_size = 30
-  }
+
   user_data = templatefile("./tools-install.sh", {})
 
   tags = {
